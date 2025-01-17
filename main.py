07e17476b89c7f9011b9c6e57bf70dc1f7bc3841
Aurimas <aurimasnarkevicius@gmail.com>
2025-01-17 08:34:24 +0100

    Updated movement as well as how things can be grouped.

--- main.py
+++ main.py
@@ -11,15 +11,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for item in updatables:
+            item.update(dt)
         screen.fill((200,200,200))
-        player.draw(screen)
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
